e06b83337ddf94fc8dcda4bb1708523f9f88f5f7
Danail-Dimitrov <danail-dimitrov@outlook.com>
2025-01-29 03:15:22 +0200

    Creating a window and basic game loop

--- GameOne/GameOne/main.cpp
+++ GameOne/GameOne/main.cpp
@@ -1,21 +1,27 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::VideoMode videoMode({ 640, 480 }); // 640x480 pixels
+	std::string windowName = "GameOne"; // The name of the window
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	sf::Window window(videoMode, windowName, sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize); // Create a window with a default style. The style is the design of the window. Curently we have Titlebar, Close button and the option to resize the window.
+	
+	//Game loop
+	//While the window is open, the game will run
+	while (window.isOpen()) 
+	{
+		//Event polling. (Did something happen)
+		while (const std::optional event = window.pollEvent()) // Checks if an event occured and stores it in an optional
+		{
+			//If the event is to close the window, then close the window
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+	}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	//End of the game
+	return 0;
 }
